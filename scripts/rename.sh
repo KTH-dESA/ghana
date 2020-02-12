16aef896b7a5f908a3a4040220034de307e7363a
Will Usher <wusher@kth.se>
2020-02-12 22:51:23 +0100

    Corrected paths to work from the repository root

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -2,15 +2,13 @@
 
 otoole validate datapackage datapackage.json --config validate.yaml
 
-EMISSION_SED_FILE=emission.sed
-FUEL_SED_FILE=fuel.sed
-TECH_SED_FILE=tech.sed
-TRADE_SED_FILE=trade.sed
-
-cp data/TECHNOLOGY.csv data/TECHNOLOGY_old.csv
-
 # Create a temporary folder
 mkdir -p tmp/data
+
+EMISSION_SED_FILE=tmp/emission.sed
+FUEL_SED_FILE=tmp/fuel.sed
+TECH_SED_FILE=tmp/tech.sed
+TRADE_SED_FILE=tmp/trade.sed
 
 # Define the substitute function
 substitute () {
@@ -23,34 +21,32 @@ done
 }
 
 # Emissions
-python combine_names.py $EMISSION_SED_FILE data/EMISSION.csv countrycode.csv emissionfactor.csv
+python scripts/combine_names.py $EMISSION_SED_FILE data/EMISSION.csv rename/countrycode.csv rename/emissionfactor.csv
 # Find files with EMISSION commodities
 EMISSION_FILES=`grep "EMISSION" data/*.csv -lr`
 substitute "$EMISSION_FILES data/EMISSION.csv" $EMISSION_SED_FILE
 
 # Fuels
-python combine_names.py $FUEL_SED_FILE data/FUEL.csv countrycode.csv fuelcode.csv
+python scripts/combine_names.py $FUEL_SED_FILE data/FUEL.csv rename/countrycode.csv rename/fuelcode.csv
 # Find the files with FUEL commodities
 FUEL_FILES=`grep "FUEL" data/*.csv -lr`
 substitute "$FUEL_FILES data/FUEL.csv" $FUEL_SED_FILE
 
 # Technologies
-python combine_names.py $TECH_SED_FILE data/TECHNOLOGY_old.csv countrycode.csv techcodes.csv
+python scripts/combine_names.py $TECH_SED_FILE data/TECHNOLOGY.csv rename/countrycode.csv rename/techcodes.csv
 # Find the files with technologies
 TECH_FILES=`grep "TECHNOLOGY" data/*.csv -lr`
 substitute "$TECH_FILES data/TECHNOLOGY.csv" $TECH_SED_FILE
 
 # Trade links
-python combine_names.py $TRADE_SED_FILE data/TECHNOLOGY_old.csv countrycode.csv tradecode.csv countrycode.csv tradesuffix.csv
+python scripts/combine_names.py $TRADE_SED_FILE data/TECHNOLOGY.csv rename/countrycode.csv rename/tradecode.csv rename/countrycode.csv rename/tradesuffix.csv
 # Find the files with trade links
 TRADE_FILES=`grep "TECHNOLOGY" data/*.csv -lr`
 substitute "$TRADE_FILES data/TECHNOLOGY.csv" $TRADE_SED_FILE
 
 # Clean up
 rm -r tmp
-# rm -r *.sed
-rm -r data/TECHNOLOGY_old.csv
 
 otoole validate datapackage datapackage.json  --config validate.yaml
 
-. run.sh
+. scripts/run.sh
